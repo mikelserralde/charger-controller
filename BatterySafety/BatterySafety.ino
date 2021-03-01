2e5fb5427ee9e3c829dde25f3aec2f522a1ee421
HaydenBLong <55929898+HaydenBLong@users.noreply.github.com>
2021-03-01 16:37:33 -0600

    Update BatterySafety.ino
    Pin definitions, used Metro library for timing, now with 90% less pseudo code

--- BatterySafety/BatterySafety.ino
+++ BatterySafety/BatterySafety.ino
@@ -1,3 +1,5 @@
+
+
 /*
  * STATE MACHINE
  * 
@@ -5,95 +7,118 @@
  * 
  * 
  * TO DEFINE: 
- *  
- *  Contactor_Open (3.3V on accum)
-    Contactor_Closed (3.3V on accum)
-    Pos Contactor Sense - high if contactor closed
-    Neg Contactor Sense
-    SC After Interconnect Sense
-    AMS Fault Sense - AMS_FAULT_SENSE
-    IMD Fault Sense - IMD_FAULT_SENSE
-  
-    Pos Contactor Control - Pos_Contactor_Ctrl
-    Neg Contactor Control - Neg_Contactor_Ctrl
-    Pos Contactor Control (accum)?
-    Neg Contactor Control (accum)?
-
-    States: Not_Charging, Precharge, Charging, ??Charging Shutdown??, error state???
-    Errors: CONTACTOR_CLOSED, CONTACTOR_OPEN, PRECHARGE_TIMEOUT,  
+ *  CAN MESSAGES:
+ *    START_CHARGING, STOP_CHARGING
  */
+ 
+//#include "CAN.h"
+#include <Metro.h>
 
-#define GPT1_base _SFT_MEM8(0x401EC000
+#define NEG_CONTACTOR_CTRL   36
+#define POS_CONTACTOR_CTRL   37
+
+#define NEG_CONTACTOR_SENSE     40
+#define POS_CONTACTOR_SENSE     41
+#define CONTACTOR_OFF_3v3       18
+#define CONTACTOR_ON_3v3        19
+#define IMD_FAULT               20
+#define AMS_FAULT               21
+#define SC_STATE_3v3            23
+
+//Error_Msg
+#define CONTACTOR_OPEN          1
+#define CONTACTOR_CLOSED        2
+#define PRECHARGE_TIMEOUT       3
+#define CHARGING_STATE_ERROR    4
+
+//Charging_State_g
+#define Not_Charging            1
+#define Precharging             2
+#define Charging                3
+
+
+char Charge_State_g = Not_Charging;
+Metro EndPrecharge(1500);
+char Error_Msg = 0;
 
 void setup() {
+  pinMode(NEG_CONTACTOR_CTRL, OUTPUT);
+  pinMode(POS_CONTACTOR_CTRL, OUTPUT);
 
-    char Charge_State_g = Not_Charging;
-    GPT1_CNT = 0;
+  pinMode(AMS_FAULT, INPUT);
+  pinMode(IMD_FAULT, INPUT);
+  pinMode(POS_CONTACTOR_SENSE, INPUT);
+  pinMode(NEG_CONTACTOR_SENSE, INPUT);
+  pinMode(CONTACTOR_OFF_3v3, INPUT);
+  pinMode(CONTACTOR_ON_3v3, INPUT);
 
 }
 
 void loop() {
   // Always Check Faults --> GPIO INTERRUPT
-  if(!SC_After_Interconnect || !AMS_FAULT_SENSE || !IMD_FAULT_SENSE)
+  if(!SC_STATE_3v3 || !AMS_FAULT || !IMD_FAULT)
     {
-      Charge_State_g = Not_Charging //possibly a charge shutdown state
-      Neg_Contactor_Ctrl = 0;
-      Pos_Contactor_Ctrl = 0;
+      Charge_State_g = Not_Charging; //possibly a charge shutdown state
+      digitalWrite(NEG_CONTACTOR_CTRL, LOW);
+      digitalWrite(POS_CONTACTOR_CTRL, LOW);
     }
 
-   switch Charge_State_g:
+   switch (Charge_State_g)
    {
     case Not_Charging:
-      if(Pos_Contactor_Sense || Neg_Contactor_Sense || Open_Contactor || !Closed_Contactor)
+      if(digitalRead(POS_CONTACTOR_SENSE) || digitalRead(NEG_CONTACTOR_SENSE) || digitalRead(CONTACTOR_OFF_3v3) || !digitalRead(CONTACTOR_ON_3v3))
       {
-        return CONTACTOR_CLOSED;
+        Error_Msg = CONTACTOR_CLOSED;
       }
       //How exactly will this work? Global command variable? Change state outside of this function? 
-      if (CAN_Receive() = START_CHARGING)
+      //if (CAN_Receive() = START_CHARGING)
+      if (HIGH)
       {
-        Neg_Contactor_Ctrl = 1;
-        Pos_Contactor_Ctrl = 1;
+        digitalWrite(NEG_CONTACTOR_CTRL, HIGH);
+        digitalWrite(POS_CONTACTOR_CTRL, HIGH);
         Charge_State_g = Precharging;
+        EndPrecharge.reset();
         //START PRECHARGING TIMEOUT TIMER HERE
       }
       break;
-    case Precharge:
+    case Precharging:
       
-      if (Pos_Contactor_Sense && Neg_Contactor_Sense && Open_Contactor && !Closed_Contactor)
+      if (digitalRead(POS_CONTACTOR_SENSE) && digitalRead(NEG_CONTACTOR_SENSE) && digitalRead(CONTACTOR_OFF_3v3) && !digitalRead(CONTACTOR_ON_3v3))
       {
         Charge_State_g = Charging;
         //reset precharging timer
       }
-      else if (TIMER_COUNT > 1500)
+      else if (EndPrecharge.check())
       {
-        //reset precharging timer
-        return PRECHARGE_TIMEOUT;
+        Error_Msg = PRECHARGE_TIMEOUT;
+        Charge_State_g = Not_Charging;
       }
        //How exactly will this work? Global command variable? Change state outside of this function? 
-      else if (CAN_Receive() = STOP_CHARGING)
+      //else if (CAN_Receive() = STOP_CHARGING)
+      else if (LOW)
       {
-        Neg_Contactor_Ctrl = 0;
-        Pos_Contactor_Ctrl = 0;
+        digitalWrite(NEG_CONTACTOR_CTRL, LOW);
+        digitalWrite(POS_CONTACTOR_CTRL, LOW);
         Charge_State_g = Not_Charging;
-        //Reset precharging timer
       }
       
       break;
 
     case Charging:
-      if (!Pos_Contactor_Sense || !Neg_Contactor_Sense || !Open_Contactor || Closed_Contactor)
+      if (!digitalRead(POS_CONTACTOR_SENSE) || !digitalRead(NEG_CONTACTOR_SENSE) || !digitalRead(CONTACTOR_OFF_3v3) || digitalRead(CONTACTOR_ON_3v3))
       {
-        Charging_State_g = Not_Charging;
-        Neg_Contactor_Ctrl = 0;
-        Pos_Contactor_Ctrl = 0;
-        return CONTACTOR_OPEN;
+        Charge_State_g = Not_Charging;
+        digitalWrite(NEG_CONTACTOR_CTRL, LOW);
+        digitalWrite(POS_CONTACTOR_CTRL, LOW);
+        Error_Msg = CONTACTOR_OPEN;
       }
 
       //How exactly will this work? Global command variable? Change state outside of this function? 
-      if (CAN_Receive() = STOP_CHARGING)
+      //if (CAN_Receive() = STOP_CHARGING)
+      if (LOW)
       {
-        Neg_Contactor_Ctrl = 0;
-        Pos_Contactor_Ctrl = 0;
+        digitalWrite(NEG_CONTACTOR_CTRL, LOW);
+        digitalWrite(POS_CONTACTOR_CTRL, LOW);
         Charge_State_g = Not_Charging;
         // Checking that contactors are actually open right here, Not_Charging, or another state?
       }
@@ -101,7 +126,7 @@ void loop() {
       break;
 
     default:
-      return CHARGING_STATE_ERROR;
+      Error_Msg = CHARGING_STATE_ERROR;
       break;
    }
   
